f01565e2d9c80044d61f3fd1d7741494eb811e0c
Andreas Gut <andreasgut@gmx.ch>
2024-08-14 16:39:38 +0200

    Participant mit unterklassen verknüpft

--- src/main/java/edu/andreasgut/MuehleWebSpringVue/Models/PlayerAndSpectator/Participant.java
+++ src/main/java/edu/andreasgut/MuehleWebSpringVue/Models/PlayerAndSpectator/Participant.java
@@ -2,10 +2,13 @@ package edu.andreasgut.MuehleWebSpringVue.Models.PlayerAndSpectator;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
 import java.util.UUID;
 
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 public abstract class Participant {
 
 
