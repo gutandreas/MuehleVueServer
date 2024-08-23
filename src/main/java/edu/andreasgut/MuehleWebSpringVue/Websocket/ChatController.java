83f311fe9ea1b0c3591b9337df28c2bea2cd62ce
Andreas Gut <andreasgut@gmx.ch>
2024-08-23 14:36:17 +0200

    Grundlegendes für Chatfunktion eingebaut

--- src/main/java/edu/andreasgut/MuehleWebSpringVue/Websocket/ChatController.java
+++ src/main/java/edu/andreasgut/MuehleWebSpringVue/Websocket/ChatController.java
@@ -3,6 +3,8 @@ package edu.andreasgut.MuehleWebSpringVue.Websocket;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import edu.andreasgut.MuehleWebSpringVue.Services.SenderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -14,21 +16,25 @@ public class ChatController {
 
     SenderService senderService;
 
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
     public ChatController(SenderService senderService) {
         this.senderService = senderService;
     }
 
+
     @MessageMapping("/chat/{gameCode}/messages")
     @SendTo("/topic/chat/{gameCode}/messages")
-    public String handleMessage(@Payload String message, @DestinationVariable("gameCode") String gameCode) {
+    public String handleMessage(@Payload String data, @DestinationVariable("gameCode") String gameCode) {
         // Hier kannst du die Nachricht entsprechend verarbeiten
-        System.out.println(message);
-        //JsonObject jsonObject = JsonParser.parseString(message).getAsJsonObject();
-
+        System.out.println(data);
+        JsonObject jsonObject = JsonParser.parseString(data).getAsJsonObject();
+        String name = jsonObject.get("name").getAsString();
+        String message = jsonObject.get("message").getAsString();
+        logger.info("Nachricht von " + name + " in Game " + gameCode + ": " + message);
 
 
         // Die Nachricht wird an alle Abonnenten dieses Channels gesendet
-        return message;
+        return data;
     }
 
 
