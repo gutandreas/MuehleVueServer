3964cc645a076f7fc442763c2635c0e49247b615
Andreas Gut <andreasgut@gmx.ch>
2024-09-08 14:12:16 +0200

    einfache evaluate Methode hinzugefügt

---
+++ src/main/java/edu/andreasgut/MuehleWebSpringVue/AlphaBeta/GameTree.java
@@ -0,0 +1,18 @@
+package edu.andreasgut.MuehleWebSpringVue.AlphaBeta;
+
+import edu.andreasgut.MuehleWebSpringVue.Models.Game;
+
+public class GameTree {
+
+    public static int evaluateGame(Game game, int index){
+        int ownIndex = index;
+        int enemyIndex = ownIndex == 1? 2: 1;
+        int numberOfOwnStones = game.getBoard().getNumberOfStonesOfPlayerWithIndex(ownIndex);
+        int numberOfEnemyStones = game.getBoard().getNumberOfStonesOfPlayerWithIndex(enemyIndex);
+        int score = numberOfOwnStones - numberOfEnemyStones;
+
+        System.out.println(score);
+
+        return score;
+    }
+}
